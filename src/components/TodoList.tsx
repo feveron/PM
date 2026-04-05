1ebcbc0cda2707273f2fae6dcecdeeeea475dad1
feveron <veronika.fedyk06@gmail.com>
2026-04-05 23:05:02 +0300

    feat: enhance toDoList interface

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,7 +4,7 @@ import { TodoItem } from "./TodoItem";
 export function ToDoList({ todos }: TodoList) {
   return (
     <div className="w-full p-4">
-      <h1 className="text-2xl font-bold text-black mb-4 underline">My tasks:</h1>
+      <h1 className="text-3xl mb-4 font-bold text-transparent [-webkit-text-stroke:1px_black]">You have <span className="text-orange-300">{todos.length}</span> tasks:</h1>
       {todos.map((todo) => (
         <div key={todo.id} className="mb-4">
           <TodoItem id={todo.id} title={todo.title} description={todo.description} done={todo.done} />
