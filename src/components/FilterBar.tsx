48dde9caa008bc0e0fdec0e17939db548f12a24d
feveron <veronika.fedyk06@gmail.com>
2026-05-11 04:08:49 +0300

    feat: integrate PostHog for task tracking and analytics

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,3 +1,6 @@
+import { posthog } from "posthog-js";
+import { useEffect, useState } from "react";
+
 type Props = {
   filter: "all" | "active" | "completed";
   setFilter: (value: "all" | "active" | "completed") => void;
@@ -6,35 +9,47 @@ type Props = {
 export function FilterBar({ filter, setFilter }: Props) {
   const base =
     "px-3 py-1 text-2xl font-bold [-webkit-text-stroke:1px_black] cursor-pointer transition";
+  const [showFlag, setShowFlag] = useState(false);
+
+  useEffect(() => {
+    posthog.onFeatureFlags(() => {
+      setShowFlag(!!posthog.isFeatureEnabled("show-urgent-filter"))
+    });
+  }, []);
 
   return (
     <div className="flex px-4 justify-start w-full gap-2">
       <button
-        className={`${base} ${
-          filter === "all" ? "text-orange-300" : "text-transparent"
-        }`}
+        className={`${base} ${filter === "all" ? "text-orange-300" : "text-transparent"
+          }`}
         onClick={() => setFilter("all")}
       >
         All
       </button>
-        <div className="w-1 border rounded border-orange-900 bg-transparent" />
+      <div className="w-1 border rounded border-orange-900 bg-transparent" />
       <button
-        className={`${base} ${
-          filter === "active" ? "text-orange-300" : "text-transparent"
-        }`}
+        className={`${base} ${filter === "active" ? "text-orange-300" : "text-transparent"
+          }`}
         onClick={() => setFilter("active")}
       >
         Active
       </button>
-        <div className="w-1 border rounded border-orange-900 bg-transparent" />
+      <div className="w-1 border rounded border-orange-900 bg-transparent" />
       <button
-        className={`${base} ${
-          filter === "completed" ? "text-orange-300" : "text-transparent"
-        }`}
+        className={`${base} ${filter === "completed" ? "text-orange-300" : "text-transparent"
+          }`}
         onClick={() => setFilter("completed")}
       >
         Done
       </button>
+      {showFlag && (<>
+        <div className="w-1 border rounded border-orange-900 bg-transparent" />
+        <button className={`${base} ${filter === "completed" ? "text-orange-300" : "text-transparent"
+          }`}>
+          Urgent tasks
+        </button>
+      </>
+      )}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import posthog from 'posthog-js'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://da779397b72c1a6403d2d365be27ebbb@o4511345402707968.ingest.de.sentry.io/4511345407098961",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  environment: "development",
+  sendDefaultPii: true
+
+})
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  capture_pageview: true,
+  session_recording: {
+    maskAllInputs: false,
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
